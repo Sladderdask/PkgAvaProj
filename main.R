e584bc61bcf37eebf76425ccf4025cb529b2fffb
Mosiiogren <moa.ogren@tegnet.se>
2025-05-14 13:23:49 +0200

    Code for uploading binary sequences to database

--- main.R
+++ main.R
@@ -23,13 +23,13 @@ dbListFields(conn, "GeCKO")
 
 # Adding data to database
 selected_data <- sgRNA_data[, c("sgrna", "LFC", "score")]
-dbWriteTable(conn, "sgRNA_data", selected_data, overwrite = TRUE, row.names=FALSE)
+dbWriteTable(conn, "sgRNA_data", selected_data, overwrite = TRUE, column.names=FALSE)
 
 selected_data <- sgRNA_A[, c("UID", "seq")]
-dbWriteTable(conn, "GeCKO", selected_data, overwrite = TRUE, row.names=FALSE)
+dbWriteTable(conn, "GeCKO", selected_data, overwrite = TRUE, column.names=FALSE)
 
 selected_data <- sgRNA_B[, c("UID", "seq")]
-dbWriteTable(conn, "GeCKO", selected_data, append = TRUE, row.names=FALSE)
+dbWriteTable(conn, "GeCKO", selected_data, append = TRUE, column.names=FALSE)
 
 
 # Peek into database
@@ -37,9 +37,9 @@ head(dbReadTable(conn, "GeCKO"))
 tail(dbReadTable(conn, "GeCKO"))
 
 # Reqrite seqeunces into Binary seqs to one hot encoding
-sequences <- dbGetQuery(conn, "SELECT seq FROM GeCKO")
+gecko_df <- dbGetQuery(conn, "SELECT * FROM GeCKO")
 # Take the sequences from the column seq
-sequences <- sequences$seq
+sequences <- gecko_df$seq
 # Create dictionary
 one_hot_map <- c("0001", "0010", "0100", "1000")
 names(one_hot_map) <- c("A", "C", "G", "T")
@@ -51,9 +51,22 @@ dna_to_one_hot <- function(seqs){
   # Convert each base using the map
   sapply(split_seqs, function(bases) paste(one_hot_map[bases], collapse = ""))
 }
+
 # Call on the function
-binary_results <- dna_to_one_hot(sequences)
-print(binary_results)
+onehotresult <- dna_to_one_hot(sequences)
+
+# Add to dataframe gecko_df
+new_dataframe <- cbind(gecko_df, onehotseq=onehotresult)
+# Add to datbase table GeCKO
+dbWriteTable(conn, "GeCKO", new_dataframe, overwrite = TRUE, column.names=FALSE)
+# Verify the update
+gecko_df <- dbGetQuery(conn, "SELECT * FROM GeCKO")
+head(gecko_df)
+
+
+
+
+
 
 # Disconnect from database
 dbDisconnect(conn)
