7093ae772cade81b5a4d6cdbad417e917feac470
Dennis Harding <dennis.harding2@gmail.com>
2025-05-19 11:57:17 +0200

    Adding onehot columnames in GeCKO table.

--- DatabasLite.py
+++ DatabasLite.py
@@ -14,6 +14,26 @@ CREATE TABLE IF NOT EXISTS sgRNA_data(
 CREATE TABLE GeCKO(
   UID VARCHAR PRIMARY KEY,
   Sequence TEXT,
+  nt1 TEXT, 
+  nt2 TEXT, 
+  nt3 TEXT, 
+  nt4 TEXT, 
+  nt5 TEXT, 
+  nt6 TEXT, 
+  nt7 TEXT, 
+  nt8 TEXT, 
+  nt9 TEXT, 
+  nt10 TEXT, 
+  nt11 TEXT, 
+  nt12 TEXT, 
+  nt13 TEXT, 
+  nt14 TEXT, 
+  nt15 TEXT, 
+  nt16 TEXT, 
+  nt17 TEXT, 
+  nt18 TEXT, 
+  nt19 TEXT, 
+  nt20 TEXT,
   FOREIGN KEY (UID) REFERENCES sgRNA_DATA (sgRNAid)
   
 );
