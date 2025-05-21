eb910f9e8e6878d64ea543d0e0ddc6a478691cc0
Dennis Harding <dennis.harding2@gmail.com>
2025-05-21 14:07:22 +0200

    Shiny app update.

--- shiny/app.R
+++ shiny/app.R
@@ -1,25 +1,65 @@
+library(shiny)
+
+ui <- fluidPage(
+  titlePanel("Avancerad Bioinformatik Web"),
+  
+  tabsetPanel(
+    id = "main_tabs",
+    
+    tabPanel("Plot Viewer",
+             sidebarLayout(
+               sidebarPanel(
+                 selectInput("plot_choice", "Choose a plot to display:",
+                             choices = c("RndForestClass" = "Rplot.png",
+                                         "RndForestReg" = "Rplot01.png"))
+               ),
+               mainPanel(
+                 imageOutput("plot_image")
+               )
+             )
+    ),
+    
+    tabPanel("Data Tools",
+             sidebarLayout(
+               sidebarPanel(
+                 textInput("user_name", "Enter your name:"),
+                 actionButton("submit", "Submit")
+               ),
+               mainPanel(
+                 textOutput("greeting")
+               )
+             )
+    )
+  )
+)
 server <- function(input, output, session) {
   
-  access_granted <- reactiveVal(FALSE)
   tabs_added <- reactiveVal(FALSE)  # Track if secret tabs are already added
+  
+  Valid_users <- c("dennis", "moa")
   
   observeEvent(input$submit, {
     if (tolower(input$user_name) == "dennis") {
       
-      access_granted(TRUE)
-      
       # Only add tabs if not already added
       if (!tabs_added()) {
         insertTab(inputId = "main_tabs",
-                  tabPanel("Welcome", h3("Welcome, admin!")),
+                  tabPanel("Welcome",
+                           sidebarLayout(
+                             sidebarPanel(
+                               h3("Welcome, admin!"),
+                               actionButton("lock", "Lock")
+                             ),
+                             mainPanel(
+                               textOutput("hello")
+                             )
+                           )
+                           
+                  ),  
                   target = "Data Tools",
                   position = "after",
-                  select = TRUE)
-        
-        insertTab(inputId = "main_tabs",
-                  tabPanel("Secret Tab", h4("You unlocked this tab, wooo hidden info!")),
-                  target = "Welcome",
-                  position = "after")
+                  select = TRUE
+        )
         
         tabs_added(TRUE)
       }
@@ -37,6 +77,13 @@ server <- function(input, output, session) {
     })
   })
   
+  observeEvent(input$lock, {
+    removeTab(inputId= "main_tabs",
+              target= "Welcome"
+    )
+    tabs_added(FALSE)
+  })
+  
   output$plot_image <- renderImage({
     list(
       src = file.path("www", input$plot_choice),
@@ -46,3 +93,7 @@ server <- function(input, output, session) {
     )
   }, deleteFile = FALSE)
 }
+
+
+
+shinyApp(ui, server)
