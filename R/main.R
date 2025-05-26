982a9cbca763c387a6aefba5b86cd81f2f931988
Dennis Harding <dennis.harding2@gmail.com>
2025-05-26 12:47:59 +0200

    FOlder containing r files. These are example files.

---
+++ R/main.R
@@ -0,0 +1,15 @@
+#' FunTitle
+#'
+#' @param a
+#'
+#' @returns
+#' Returns 69
+#' TRUE
+#'
+#' @examples
+#' fun()
+#' @export
+fun <- function(a){
+  a <- 69
+}
+
---
+++ R/roxyex.R
@@ -0,0 +1,11 @@
+#' Title
+#'
+#' @param a
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+fun2 <- function(a){
+  a <- 69
+}
