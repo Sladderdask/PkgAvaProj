176028e9540267329a4c53045a3ff7b540c70a21
Mosiiogren <moa.ogren@tegnet.se>
2025-05-30 20:11:22 +0200

    Added import for cleaner writing in machine learning file R.

---
+++ src/imports.py
@@ -0,0 +1,10 @@
+# Ta in rätt paket
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+import matplotlib.pyplot as plt
+import sqlite3
+import pandas as pd
+import numpy as np
+import shap
